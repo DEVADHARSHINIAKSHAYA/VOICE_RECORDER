91fe646f37deef6d391aaa090f34ab1c60fc4293
DEVADHARSHINI A <128233650+DEVADHARSHINIAKSHAYA@users.noreply.github.com>
2023-09-10 12:15:22 +0530

    Add files via upload
    We can use python's sounddevice module to record and play audio. This module along with the wavio or the scipy module provides a way to save recorded audio.

---
+++ recorder.py
@@ -0,0 +1,11 @@
+import sounddevice
+from scipy.io.wavfile import write
+
+def voice_recorder(seconds, file):
+    print("Recording Started…")
+    recording = sounddevice.rec((seconds * 44100), samplerate= 44100, channels=2)
+    sounddevice.wait()
+    write(file, 44100, recording)
+    print("Recording Finished")
+
+voice_recorder(10, "record.wav")
